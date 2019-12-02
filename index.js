a8b41130b9c28b1fdb0146ea22bb3cfe84981e95
Martin Janiczek <martin@janiczek.cz>
2019-12-02 18:45:12 +0100

    Add extra lines wrapping

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {channel} = require('./lib');
 const colors = require('colors');
+const {stripColors} = colors;
+const size = require('window-size');
 
 /////////////////////////////////////////////
 
@@ -7,69 +9,79 @@ const testColors = () => console.log(
  '0'.black + '1'.red + '2'.green + '3'.yellow + '4'.blue + '5'.magenta + '6'.cyan + '7'.white + '8'.gray
 );
 
-const debug = (msg) => console.log(
+const wrap = (str) => {
+  const stringLength = stripColors(str).length;
+  const terminalSize = size.get();
+  const lineLength = Math.min(stringLength, terminalSize.width);
+  const lines = '-'.repeat(lineLength).gray;
+  return `${lines}\n${str}\n${lines}`;
+};
+
+
+const debug = (msg) => console.log(wrap(
   '[Script debug] '.gray + msg.green
-);
+));
 
-const info = (msg) => console.log(
+const info = (msg) => console.log(wrap(
   '['.gray + 'Script'.green + '] '.gray + msg.green
-);
+));
 
-const error = (msg) => console.log(
+const error = (msg) => console.log(wrap(
   '['.gray + 'Script error'.red + '] '.gray + msg.green
-);
+));
 
+// chat is the only thing that doesn't get extra '--------' lines
 const chat = ({viewerName,message}) => console.log(
   viewerName.cyan + ': '.gray + message
 )
 
-const cheer = ({viewerName,amount,message}) => console.log(
+const cheer = ({viewerName,amount,message}) => console.log(wrap(
   '['.gray + 'Cheer'.yellow + '] '.gray + viewerName.cyan + ' cheered '.green + amount.yellow + ' bits: '.green + message
-);
+));
 
-const sub = ({viewerName,message,plan}) => console.log(
+const sub = ({viewerName,message,plan}) => console.log(wrap(
   '['.gray + `Sub: ${plan}`.yellow + '] '.gray + viewerName.cyan + ' subscribed: '.green + message
-);
+));
 
-const resub = ({viewerName,message,months,plan}) => console.log(
+const resub = ({viewerName,message,months,plan}) => console.log(wrap(
   '['.gray + `Resub: ${plan}`.yellow + '] '.gray + viewerName.cyan + ' resubscribed for '.green + months.yellow + ' months: '.green + message
-);
+));
 
-const subgift = ({viewerName,recipientName,plan}) => console.log(
+const subgift = ({viewerName,recipientName,plan}) => console.log(wrap(
   '['.gray + `Subgift: ${plan}`.yellow + '] '.gray + viewerName.cyan + ' gifted sub to '.green + recipientName.cyan
-);
+));
 
-const host = ({viewerName,viewers}) => console.log(
+const host = ({viewerName,viewers}) => console.log(wrap(
   '['.gray + 'Host'.magenta + '] '.gray + viewerName.cyan + ' hosted with '.green + viewers.magenta + ' viewers'.green
-);
+));
 
-const raid = ({viewerName,viewers}) => console.log(
+const raid = ({viewerName,viewers}) => console.log(wrap(
   '['.gray + 'Raid'.magenta + '] '.gray + viewerName.cyan + ' raided with '.green + viewers.magenta + ' viewers'.green
-);
+));
 
-const follow = ({viewerName}) => console.log(
+const follow = ({viewerName}) => console.log(wrap(
   '['.gray + 'Follow'.magenta + '] '.gray + viewerName.cyan + ' started following'.green
-);
+));
 
-const ban = ({viewerName}) => console.log(
+const ban = ({viewerName}) => console.log(wrap(
   '['.gray + 'Ban'.red + '] '.gray + viewerName.cyan + ' got banned'.green
-);
+));
 
-const streamBegin = ({game}) => console.log(
+const streamBegin = ({game}) => console.log(wrap(
   '['.gray + 'Stream begun'.blue + '] '.gray + 'Theme is: '.green + game.blue
-);
+));
 
-const streamChangeGame = ({game}) => console.log(
+const streamChangeGame = ({game}) => console.log(wrap(
   '['.gray + 'Stream changed'.blue + '] '.gray + 'New theme is: '.green + game.blue
-);
+));
 
-const streamEnd = () => console.log(
+const streamEnd = () => console.log(wrap(
   '['.gray + 'Stream ended'.blue + ']'.gray
-);
+));
 
-const streamlabsDonation = ({viewerName,amount,currency,message}) => console.log(
+const streamlabsDonation = ({viewerName,amount,currency,message}) => console.log(wrap(
   '['.gray + 'Donation'.yellow + '] '.gray + viewerName.cyan + ' donated '.green + `${amount} ${currency}`.yellow + ': '.green + message
-);
+));
 
 /////////////////////////////////////////////
 
